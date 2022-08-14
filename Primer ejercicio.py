7583d0f0ce7e804be2923ca8085ed5bd116a5276
MartinianoLurgo <martinianolurgo@gmail.com>
2022-08-14 16:38:13 -0300

    Realizacion de tres actividades en py

---
+++ Primer ejercicio.py
@@ -0,0 +1,13 @@
+#Cada estudiante deberá  resolver  UN (1) ejercicio a elección (diferentes entre cada uno de los 
+# integrantes) de los 10 ejercicios propuestos por el docente y subir en su propia rama del GitHub.
+
+
+#Escribir el código en Python que permita realizar lo siguiente:
+#1) Crear una lista en Python denominada “Dueno” que contenga los siguientes valores:
+#  28957346,  Juan,  Perez, 4789689,  Belgrano 101
+
+Dueno = [ 28957346,  "Juan",  "Perez", 4789689,  "Belgrano 101"]
+if Dueno[0]> 26000000:
+    print("El numero de telefono del usuario es:",Dueno[3])
+else:
+    print()
---
+++ Segundo ejercicio.py
@@ -0,0 +1,14 @@
+#2)   Crear una lista en Python denominada “Dueno2”  y recorrerla con un bucle  mostrando 
+# sus elementos por pantalla con excepción del DNI y el apellido. Los elementos de la lista son:
+
+
+Dueno2 =   [23546987,  "Maria",  "Perez", 4789689,  "Pueyrredon  811"]
+
+print(
+"Documento:",Dueno2[0],"\n"
+"Nombbre:",Dueno2[1],"\n"
+"Apellido:",Dueno2[2],"\n"
+"Telefono:",Dueno2[3],"\n"
+"Direccion:",Dueno2[4])
+
+
