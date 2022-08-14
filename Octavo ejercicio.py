de381ee89afd03b9d600a6501f1f32d2e9e9e5ac
MartinianoLurgo <martinianolurgo@gmail.com>
2022-08-14 18:57:02 -0300

    octavo y noveno ejercicio realizado

---
+++ Octavo ejercicio.py
@@ -0,0 +1,14 @@
+#8) Crear una tupla en Python con el nombre de “Historial4” la cual contenga los siguientes valores:
+
+historia4 = {7510, 7960, 76180, 800, 4100}
+
+#Que representa montos de atención en pesos por diferentes
+#servicios/consultas de la mascota “Olivia”. Cree una 
+#función para determinar el valor mínimo de atención gastada
+#en “Olivia”  mostrándolo en pantalla.
+
+def minimo():
+    print("El minimo es de:",min(historia4))
+minimo()
+
+    
